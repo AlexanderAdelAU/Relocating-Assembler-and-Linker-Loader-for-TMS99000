4c99ec1f82702b4e5361e335b7fea3b3996bafd7
adel314 <38694473+adel314@users.noreply.github.com>
2021-01-22 10:58:10 +1030

    Delete make_cTest99.bat

--- r99/make_cTest99.bat
+++
@@ -1,8 +0,0 @@
-smallcp -C cTest99
-R99 cTest99 SCHCLC
-link99  -M  -S cTest99 clib99.LIB iolib99.LIB 
-REM link99 -M cTest99 abc
-REM link99 -M cFTest cfloat
-REM  drel cTest99.R99
-REM=========================
-REM smallcp -C SimpsonsRule
